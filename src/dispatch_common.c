82ef8e171cc7d2433a2554fdf5461fc50f8ced13
Kenney Phillis Jr <kphillisjr@gmail.com>
2014-02-27 17:26:52 -0600

    move DllMain to src/dispatch_common.c
    MSVC does not support " __attribute__((constructor))". The closest place to put this is in DllMain.

--- src/dispatch_common.c
+++ src/dispatch_common.c
@@ -158,14 +158,74 @@ static struct api api = {
 
 static bool library_initialized;
 
+#ifndef _WIN32
 static void
 library_init(void) __attribute__((constructor));
+#else
+static void
+library_init(void);
+#endif
 
 static void
 library_init(void)
 {
     library_initialized = true;
 }
+
+#ifndef _WIN32
+/**
+ * This global symbol is apparently looked up by Windows when loading
+ * a DLL, but it doesn't declare the prototype.
+ */
+BOOL WINAPI
+DllMain(HINSTANCE dll, DWORD reason, LPVOID reserved);
+
+BOOL WINAPI
+DllMain(HINSTANCE dll, DWORD reason, LPVOID reserved)
+{
+    void *data;
+
+    switch (reason) {
+    case DLL_PROCESS_ATTACH:
+        gl_tls_index = TlsAlloc();
+        if (gl_tls_index == TLS_OUT_OF_INDEXES)
+            return FALSE;
+        wgl_tls_index = TlsAlloc();
+        if (wgl_tls_index == TLS_OUT_OF_INDEXES)
+            return FALSE;
+
+        /* FALLTHROUGH */
+
+    case DLL_THREAD_ATTACH:
+        data = LocalAlloc(LPTR, gl_tls_size);
+        TlsSetValue(gl_tls_index, data);
+
+        data = LocalAlloc(LPTR, wgl_tls_size);
+        TlsSetValue(wgl_tls_index, data);
+
+        epoxy_handle_external_wglMakeCurrent();
+        library_init();
+        break;
+
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        data = TlsGetValue(gl_tls_index);
+        LocalFree(data);
+
+        data = TlsGetValue(wgl_tls_index);
+        LocalFree(data);
+        break;
+
+        if (reason == DLL_PROCESS_DETACH) {
+            TlsFree(gl_tls_index);
+            TlsFree(wgl_tls_index);
+        }
+        break;
+    }
+
+    return TRUE;
+}
+#endif
 
 static void
 get_dlopen_handle(void **handle, const char *lib_name, bool exit_on_fail)
